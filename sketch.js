79575963e7bcfb45765f6cb75764ba6ab7dd2783
BHABESH-PATTNAIK <87313880+BHABESH-PATTNAIK@users.noreply.github.com>
2021-07-18 22:54:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,38 @@
+var ship, ship_running,edges;
+var seaImage;
+var sea;
 
-function setup() {
-  createCanvas(400,400);
-  var ball=createSprite(200,200,40,40);
+
+function preload(){
+ship_running=loadAnimation ("ship1.png","ship2.png","ship3.png","ship4.png");
+seaImage = loadImage("sea.png")
+
+
 }
 
-function draw() 
-{
-  background(4);
-drawSprites();
+function setup(){
+  createCanvas(600,200);
+  // creating ship
+  ship = createSprites()
+  ship.addAnimation("running",ship_running)
+  edges = createEdgeSprites()
+
+  //adding scale and position to ship
+  ship.scale =1
+  ship.x=40
+
+  //creating sea 
+  sea =createSprites(300,180,600,50)
+  sea.addImage ("sea",seaImage)
 }
 
+function draw() {
+  background("white");
 
+  sea.velocityX =-2
 
+if(sea.x<0)
+{sea.x =sea.width/2}
 
+ drawSprites
+}
